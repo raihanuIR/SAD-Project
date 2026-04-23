2c64a2f73f06e7072a4b18bbd174b68965e4456f
raihanulR <raihanul409@gmail.com>
2026-04-23 06:33:46 +0600

    switched to prisma 7 direct adapter

--- raihanshop-api/lib/prisma.js
+++ raihanshop-api/lib/prisma.js
@@ -1,18 +1,16 @@
 // lib/prisma.js
 import { PrismaClient } from '../generated/client';
-import * as LibSQLAdapter from '@prisma/adapter-libsql';
-import { createClient } from '@libsql/client';
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
 
 const globalForPrisma = globalThis;
-const PrismaLibSQL = LibSQLAdapter.PrismaLibSQL;
 
 function createPrisma() {
     if (process.env.TURSO_DATABASE_URL && process.env.TURSO_AUTH_TOKEN) {
-        const libsql = createClient({
+        // Prisma 7+ direct instantiation
+        const adapter = new PrismaLibSQL({
             url: process.env.TURSO_DATABASE_URL,
             authToken: process.env.TURSO_AUTH_TOKEN,
         });
-        const adapter = new PrismaLibSQL(libsql);
         return new PrismaClient({ adapter });
     }
     return new PrismaClient();
