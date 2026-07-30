f6537e3d4f43cf31dbd8962422d120b9b72a421e
raihanulR <raihanul409@gmail.com>
2026-07-30 21:51:29 +0600

    Remove duplicate next.config.ts

--- raihanshop-api/generated/client/edge.js
+++ raihanshop-api/generated/client/edge.js
@@ -225,7 +225,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
   "clientVersion": "6.4.1",
@@ -234,7 +235,7 @@ const config = {
     "db"
   ],
   "activeProvider": "sqlite",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- raihanshop-api/generated/client/index.js
+++ raihanshop-api/generated/client/index.js
@@ -226,7 +226,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
   "clientVersion": "6.4.1",
@@ -235,7 +236,7 @@ const config = {
     "db"
   ],
   "activeProvider": "sqlite",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
